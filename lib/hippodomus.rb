69f4b4d04cfc84896712e1bb887deb4bbf07115d
pezholio <pezholio@gmail.com>
2014-11-10 09:14:26 +0000

    Export JSON too

--- lib/hippodomus.rb
+++ lib/hippodomus.rb
@@ -17,33 +17,6 @@ table.css("tr").each do |row|
   areas << row.css("td").first.inner_text rescue nil
 end
 
-# Export addresses by postcode area
-areas.each do |area|
-  command = "mongoexport --host #{ENV['MONGO_HOST']} --db #{ENV['MONGO_DB']} --collection addresses --csv --fields pao,sao,street,locality,town,postcode --out addresses/#{area}.csv --sort \"{postcode: 1}\" --query \"{ postcode: /^#{area}.*/i }\""
-  command << " --username #{ENV['MONGO_USERNAME']} " if ENV['MONGO_USERNAME']
-  command << " --password #{ENV['MONGO_PASSWORD']} " if ENV['MONGO_PASSWORD']
-  `#{command}`
-end
-
-# Zip CSVs by letter
-("A".."Z").each do |letter|
-  files = Dir.glob("./addresses/#{letter}*")
-  if files.count > 0
-    Zip::File.open("./addresses/#{letter}.zip", Zip::File::CREATE) do |zipfile|
-      files.each do |file|
-        zipfile.add(File.basename(file), file)
-      end
-    end
-  end
-end
-
-# Zip all the zips
-Zip::File.open("./addresses/addresses.zip", Zip::File::CREATE) do |zipfile|
-  Dir.glob("./addresses/*.zip").each do |file|
-    zipfile.add(File.basename(file), file)
-  end
-end
-
 # Connect to AWS
 connection = Fog::Storage.new({
   :provider                 => 'AWS',
@@ -51,24 +24,59 @@ connection = Fog::Storage.new({
   :aws_secret_access_key    => ENV['AWS_SECRET_ACCESS_KEY']
 })
 
-directory = connection.directories.get("open-addresses")
-file = directory.files.get("addresses.zip")
+# Export addresses by postcode area
 
-# Backup old file
-directory.files.create(
-  :key    => "addresses-#{DateTime.now.to_s}.zip",
-  :body   => file.body,
-  :public => true
-)
+{
+  csv: "csv",
+  json: "jsonArray"
+}.each do |format, option|
 
-# Update main file
-file.body = File.open("./addresses/addresses.zip")
-file.public = true
-file.save
+  areas.each do |area|
+    command = "mongoexport --host #{ENV['MONGO_HOST']} --db #{ENV['MONGO_DB']} --collection addresses --#{option} --fields pao,sao,street,locality,town,postcode --out addresses/#{area}.#{format} --sort \"{postcode: 1}\" --query \"{ postcode: /^#{area}.*/i }\""
+    command << " --username #{ENV['MONGO_USERNAME']} " if ENV['MONGO_USERNAME']
+    command << " --password #{ENV['MONGO_PASSWORD']} " if ENV['MONGO_PASSWORD']
+    `#{command}`
+  end
 
-# Download torrent file
-open('addresses.torrent', 'wb') do |f|
-  f << open("#{file.public_url}?torrent").read
+  # Zip CSVs by letter
+  ("A".."Z").each do |letter|
+    files = Dir.glob("./addresses/#{letter}*#{format}")
+    if files.count > 0
+      Zip::File.open("./addresses/#{letter}.#{format}.zip", Zip::File::CREATE) do |zipfile|
+        files.each do |file|
+          zipfile.add(File.basename(file), file)
+        end
+      end
+    end
+  end
+
+  # Zip all the zips
+  Zip::File.open("./addresses/addresses.#{format}.zip", Zip::File::CREATE) do |zipfile|
+    Dir.glob("./addresses/*#{format}.zip").each do |file|
+      zipfile.add(File.basename(file), file)
+    end
+  end
+
+  directory = connection.directories.get("open-addresses")
+  file = directory.files.get("addresses.#{format}.zip")
+
+  # Backup old file
+  directory.files.create(
+    :key    => "addresses-#{DateTime.now.to_s}.#{format}.zip",
+    :body   => file.body,
+    :public => true
+  )
+
+  # Update main file
+  file.body = File.open("./addresses/addresses.#{format}.zip")
+  file.public = true
+  file.save
+
+  # Download torrent file
+  open("addresses.#{format}.torrent", 'wb') do |f|
+    f << open("#{file.public_url}?torrent").read
+  end
+
 end
 
 # Cleanup
